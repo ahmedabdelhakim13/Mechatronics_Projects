c0a842d9578db1b306cd99d06c07c7715f1d83ce
MostafaMohamedAbelAzeem <4ikawy.mo7med@gmail.com>
2023-10-15 17:48:14 +0300

    Documentation , MACROS And Adding features.

--- first_project/traffic_light/Task1_Traffic_Light_ozi.ino
+++ first_project/traffic_light/Task1_Traffic_Light_ozi.ino
@@ -1,76 +1,96 @@
 // Simulation link: https://www.tinkercad.com/things/5isKJcbButC
 
-// Define pin numbers for components
-#define RED_LED     8
-#define YELLOW_LED  9
-#define GREEN_LED   10
-#define BUTTON      2
-#define BUZZER      12
-#define PRESSURE    0
+                           /* MACROS */
+
+                      // Define pin numbers for components.
+
+#define RED_LED           8
+#define YELLOW_LED        9
+#define GREEN_LED        10
+#define BUTTON            2
+#define BUZZER           A1
+#define PRESSURE         A0
+#define BlinkingTime    1000
+#define CarTime         3000
+#define Repeat          1000
+                      
+                      //Variable stores the number of cars.
 
 unsigned int carsNum = 0;
 
+                       /* Set pin modes for components.  */
 void setup() 
 {
-  // Set pin modes for components
+                         // Basic components.                             
+  
+  // LEDS as output and pressure sensor as input.
   pinMode(RED_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  
-  // Attach an interrupt to the button press event
-  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, RISING);
-  
   pinMode(PRESSURE, INPUT);
+  
+                        // Added Features.
+  // Attach an interrupt to the button press event.
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, RISING);
   pinMode(BUZZER, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
-
+                       
 void loop() 
 {
-  digitalWrite(BUZZER, LOW);
+  //Turn on RED LED and Turn off all other LEDS and buzzer.
   digitalWrite(RED_LED, HIGH);
   digitalWrite(YELLOW_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);  
+  noTone( BUZZER );
+  
+  //Taking the reading of the pressure sensor.
   int value = analogRead(PRESSURE);
   
-  // Count cars based on pressure sensor reading
+  // Count cars based on pressure sensor reading.
   if (value > 200)
   {
     carsNum++;
   }
   
-  // Check if carsNum exceeds 2
+  // Check if carsNum exceeds 2.
   if (carsNum >= 2)
   {
-    // Blink the yellow LED and activate the buzzer
+    // Blink the yellow LED and activate the buzzer.
     for(int i = 0 ; i <= 3 ; i++)
     {
       digitalWrite(YELLOW_LED, !(digitalRead(YELLOW_LED)));
-      delay(1000);
+      delay(BlinkingTime);
     }
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(BUZZER, HIGH);
+    
+    // Turn off RED and Yellow LEDS.
     digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+    // Turn on Green LED and activate buzzer.
+    tone( BUZZER , 1000 );
     digitalWrite(GREEN_LED, HIGH);
     
     // Delay for a time proportional to the number of cars counted
-    delay(1000 * carsNum);
+    delay(CarTime * carsNum);
+    // Reset number of cars after all cars passes. 
     carsNum = 0;
-    
+   
     // Blink the yellow LED and reset the lights
     for(int i = 0 ; i <= 3 ; i++)
     {
       digitalWrite(YELLOW_LED, !(digitalRead(YELLOW_LED)));
-      delay(1000);
+      delay(BlinkingTime);
     }
+    // Turn off Green and Yellow LEDS and buzzer.
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(GREEN_LED, LOW);
-    digitalWrite(BUZZER, LOW);
+    noTone( BUZZER );
+    //Turn on Red LED.
     digitalWrite(RED_LED, HIGH);
   }
   
   // Regular delay before repeating the loop
-  delay(1000);
+  delay(Repeat);
 }
 
 void buttonPressed() {
@@ -79,16 +99,16 @@ void buttonPressed() {
   {
     // Alternate the yellow LED when the button is pressed
     digitalWrite(YELLOW_LED, !(digitalRead(YELLOW_LED)));
-    delay(1000);
+    delay(BlinkingTime);
   }
   
   // Add a longer delay to create a pause
   delay(2000);
   
   // Turn off the yellow LED, green LED, and buzzer, and turn on the red LED
+  noTone( BUZZER );  
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BUZZER, LOW); 
   digitalWrite(RED_LED, HIGH);
   
   // Delay for 5 seconds
