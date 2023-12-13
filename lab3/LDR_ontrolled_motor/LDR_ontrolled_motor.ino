0735a2066ea55d97b6793e25607d991225eb041c
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-12-13 17:32:51 +0200

    Create LDR_ontrolled_motor.ino

---
+++ lab3/LDR_ontrolled_motor/LDR_ontrolled_motor.ino
@@ -0,0 +1,59 @@
+#define LDR A0
+#define motor_pin 3
+float value=0.0;
+float volt=0.0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LDR,INPUT);
+  pinMode(motor_pin,OUTPUT);
+}
+
+void loop() {
+  value=analogRead(LDR);
+  Serial.println(value);
+  delay(500);
+  volt=(value/1023)*5;
+  if(volt)
+  {
+    if(volt>=4)
+    largestspeed()
+
+    else if(volt>=3)
+    Speed2()
+    else if(volt>=2)
+    speed3()
+    else if(volt>=1)
+    speed4()
+    else
+    speed5()
+  }
+  // put your main code here, to run repeatedly:
+
+}
+//defining function 
+void largestspeed()
+  {
+      analogWrite(motor_pin,256);
+      delay(1000);
+    }
+void speed2()
+    {
+      analogWrite(motor_pin,205);
+      delay(1000);
+    }
+    void speed3()
+    {
+      analogWrite(motor_pin,153);
+      delay(1000);
+    }
+    void speed4()
+    {
+      analogWrite(motor_pin,102);
+      delay(1000);
+    }
+    void speed5()
+    {
+      analogWrite(motor_pin,51);
+      delay(1000);
+    }
